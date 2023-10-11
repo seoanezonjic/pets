b561a758e9d279d6a7d85ffbd7c767807f0d1c82
seoanezonjic <darklordsone@hotmail.com>
2023-10-11 13:06:08 +0200

    Scripts migrated to cli_manager

--- src/pets/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-COMMON_OPTPARSE = os.path.abspath(os.path.join(ROOT_PATH, '..', 'pets', 'common_optparse.py'))
-REPORT_FOLDER = os.path.abspath(os.path.join(ROOT_PATH, '..', 'templates'))
-EXTERNAL_DATA = os.path.abspath(os.path.join(ROOT_PATH, '..', 'external_data'))
-HPO_FILE = os.path.join(EXTERNAL_DATA, 'hp.json')
-MONDO_FILE = os.path.join(EXTERNAL_DATA, 'mondo.obo')
-IC_FILE = os.path.join(EXTERNAL_DATA, 'uniq_hpo_with_CI.txt')
