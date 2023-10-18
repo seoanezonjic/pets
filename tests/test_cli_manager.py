b212d934b76c3b7602274bfd72c0538a40d87c01
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2023-10-18 17:33:01 +0200

    Updated testing coverage of cli_manager (added cohort_analyzer test)

--- tests/test_cli_manager.py
+++ tests/test_cli_manager.py
@@ -6,15 +6,14 @@ from subprocess import PIPE
 from pets.genomic_features import Genomic_Feature
 from pets.parsers.cohort_parser import Cohort_Parser
 from pets.cohort import Cohort
-from pets import get_gen_features, get_sorted_profs, paco_translator, profiles2phenopacket, cohort_analyzer, evidence_profiler
+from pets import get_gen_features, get_sorted_profs, paco_translator, profiles2phenopacket, cohort_analyzer, evidence_profiler, HPO_FILE, GENCODE
 import warnings
 import numpy as np
 import pytest
 from io import StringIO
 import json
+import shutil
 
-HPO_FILE = str(files('pets.external_data').joinpath('hp.json'))
-GENCODE = str(files('pets.external_data').joinpath('gencode.v43.basic.annotation.gtf.gz'))
 
 ROOT_PATH=os.path.dirname(__file__)
 DATA_TEST_PATH = os.path.join(ROOT_PATH, 'data')
@@ -22,17 +21,6 @@ SCRIPT_DATA_TEST_PATH = os.path.join(ROOT_PATH, 'input_data')
 EXPECTED_PATH = os.path.join(ROOT_PATH, 'expected')
 RETURNED_PATH = os.path.join(ROOT_PATH, 'returned')
 
-
-def capture_stdout(func):
-    def wrapper(*args, **kwargs):
-        original_stdout = sys.stdout
-        tmpfile = StringIO()
-        sys.stdout = tmpfile
-        returned = func(*args, **kwargs)
-        printed = sys.stdout.getvalue()
-        sys.stdout = original_stdout
-        return returned, printed
-    return wrapper
 
 def strng2table(strng, fs="\t", rs="\n"):
 	table = [row.split(fs) for row in strng.split(rs)][0:-1]
@@ -49,7 +37,35 @@ def test_evidence_profiler():
     assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'evidence_profiler', 'evidence_reports')}")
 
 def test_cohort_analyzer():
-    pass
+    os.makedirs(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer')}", exist_ok=True)
+    list_of_args = ["-i", f"{os.path.join(SCRIPT_DATA_TEST_PATH, 'paco_translator', 'cohort_toy_dataset.txt')}",
+                    "-o", f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'cohort_analyzer.html')}", 
+                    "-c", "chr", "-d", "patient_id", "-s", "start", "-e", "end", "-p", "phenotypes", "-S", "|",
+                    "-t", "freq", "-D", "-m", "lin", "-a", "-n"]
+    
+    #Testing first time to check if calculations can be executed without errors
+    cohort_analyzer(list_of_args)
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'cohort_analyzer.html')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'cohort_analyzer_lin_clusters.html')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'detailed_hpo_profile_evaluation.csv')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'rejected_records.txt')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'pat_hpo_matrix.npy')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'pat_hpo_matrix_x.lst')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'pat_hpo_matrix_y.lst')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'profiles_similarity_lin.txt')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'similarity_matrix_lin.npy')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'similarity_matrix_lin_x.lst')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'lin_raw_cls.npy')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'lin_linkage.npy')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'lin_clusters.txt')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'hpo_cohort_frequency.txt')}")
+    assert os.path.exists(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer', 'temp', 'cluster_asignation')}")            
+    
+    #Testing second time to check if files can be loaded (instead of doing the calculations again) without errors
+    cohort_analyzer(list_of_args)
+
+    shutil.rmtree(f"{os.path.join(RETURNED_PATH, 'cohort_analyzer')}")
 
 def test_profiles2phenopacket():
     os.environ["hpo_file"] = HPO_FILE
